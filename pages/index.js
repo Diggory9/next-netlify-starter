53b12e571d66251b422f08cbf46ae641691237e4
user4462 <nkt19910@gmail.com>
2024-04-14 15:43:57 +0700

    aaa

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,13 @@ import Navbar from '@components/Navbar'
 import { useUser } from '@auth0/nextjs-auth0/client'
 export default function Home() {
   const {user,error,isLoading} = useUser();
+  
+  return (
+    <div className="container">
+      <Navbar/>
+      <Footer />
+    </div>
+  )
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
   if (user) {
@@ -14,10 +21,4 @@ export default function Home() {
       </div>
     );
   }
-  return (
-    <div className="container">
-      <Navbar/>
-      <Footer />
-    </div>
-  )
 }
